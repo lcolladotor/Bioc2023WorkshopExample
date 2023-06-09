52edd31dd458882b28d37bab3f43b04c1bf93884
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-09 09:17:59 -0400

    Added hello() as an example function

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+#' Say hello
+#'
+#' This function is just an example function so our package can pass R CMD
+#' check.
+#'
+#' @return A `character(1)` with the word "hello".
+#' @export
+#'
+#' @examples
+#' ## Say hello ^_^
+#' hello()
+hello <- function() {
+    print("hello")
+}
